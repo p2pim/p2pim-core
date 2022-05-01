dc08e266e93d1bfd69044da0cbdcb518f4a7bf94
Fernando Rincon <f.rincon@protonmail.com>
2022-05-02 00:07:33 +0800

    refactor: sort code

--- src/cmd/daemon.rs
+++ src/cmd/daemon.rs
@@ -4,28 +4,10 @@ use clap::{Arg, ArgMatches, Command};
 use p2pim::daemon::DaemonOpts;
 
 const ARG_ETH_ADDRESS: &str = "eth.address";
+const ARG_ETH_MASTER: &str = "eth.master";
 
 const ARG_RPC_ADDRESS: &str = "rpc.address";
 const ARG_RPC_ADDRESS_DEFAULT: &str = "127.0.0.1:8122";
-
-const ARG_ETH_MASTER: &str = "eth.master";
-
-fn arg_rpc_address() -> Arg<'static> {
-  Arg::new(ARG_RPC_ADDRESS)
-    .long(ARG_RPC_ADDRESS)
-    .takes_value(true)
-    .value_name("ADDRESS")
-    .default_value(ARG_RPC_ADDRESS_DEFAULT)
-    .help("gRPC server listening address")
-}
-
-pub fn command() -> Command<'static> {
-  Command::new("daemon")
-    .about("run daemon")
-    .arg(arg_eth_address())
-    .arg(arg_rpc_address())
-    .arg(arg_eth_master())
-}
 
 fn arg_eth_address() -> Arg<'static> {
   Arg::new(ARG_ETH_ADDRESS)
@@ -44,6 +26,23 @@ fn arg_eth_master() -> Arg<'static> {
     .validator(web3::types::Address::from_str)
     .required(false)
     .help("ethereum address of the master record contract")
+}
+
+fn arg_rpc_address() -> Arg<'static> {
+  Arg::new(ARG_RPC_ADDRESS)
+    .long(ARG_RPC_ADDRESS)
+    .takes_value(true)
+    .value_name("ADDRESS")
+    .default_value(ARG_RPC_ADDRESS_DEFAULT)
+    .help("gRPC server listening address")
+}
+
+pub fn command() -> Command<'static> {
+  Command::new("daemon")
+    .about("run daemon")
+    .arg(arg_eth_address())
+    .arg(arg_eth_master())
+    .arg(arg_rpc_address())
 }
 
 pub fn run(matches: &ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
